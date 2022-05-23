341f33ed4a5d91b50a674644a65f083345e18526
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-23 10:19:40 -0400

    rename creer_tables variables

--- creer_tables.sql
+++ creer_tables.sql
@@ -1,51 +1,51 @@
 -- suppression des tables précédentes
-DROP TABLE IF EXISTS Personnes;
-DROP TABLE IF EXISTS Perissable;
-DROP TABLE IF EXISTS Sec;
-DROP TABLE IF EXISTS Quantite;
-DROP TABLE IF EXISTS Produits;
-DROP TABLE IF EXISTS Capturer;
-DROP TABLE IF EXISTS Etapes_Transitoires;
-DROP TABLE IF EXISTS Voyages;
-DROP TABLE IF EXISTS Navires;
-DROP TABLE IF EXISTS Ports_;
-DROP TABLE IF EXISTS Nations;
+DROP TABLE IF EXISTS personnes;
+DROP TABLE IF EXISTS perissable;
+DROP TABLE IF EXISTS sec;
+DROP TABLE IF EXISTS quantite;
+DROP TABLE IF EXISTS produits;
+DROP TABLE IF EXISTS capturer;
+DROP TABLE IF EXISTS etapes_transitoires;
+DROP TABLE IF EXISTS voyages;
+DROP TABLE IF EXISTS navires;
+DROP TABLE IF EXISTS ports_;
+DROP TABLE IF EXISTS nations;
 
-CREATE TABLE Nations(
-    Nationalite VARCHAR(30) PRIMARY KEY,
-    Continent VARCHAR(30)
+CREATE TABLE nations(
+    nationalite VARCHAR(30) PRIMARY KEY,
+    continent VARCHAR(30)
 );
 
-CREATE TABLE Ports_(
-Nom VARCHAR(30) PRIMARY KEY,
-Longitude DECIMAL,
-Latitude DECIMAL,
-Nationalite VARCHAR(30), 
-Taille_categorie INTEGER CHECK (Taille_categorie BETWEEN 1 AND 5) NOT NULL,
-FOREIGN KEY (Nationalite) REFERENCES Nations(Nationalite) 
+CREATE TABLE ports_(
+    nom VARCHAR(30) PRIMARY KEY,
+    longitude DECIMAL,
+    latitude DECIMAL,
+    nationalite VARCHAR(30), 
+    taille_categorie INTEGER CHECK (taille_categorie BETWEEN 1 AND 5) NOT NULL,
+    FOREIGN KEY (nationalite) REFERENCES nations(nationalite) 
 );
 
-CREATE TABLE Navires(
-    NavireID SERIAL PRIMARY KEY,
-    Navire_type VARCHAR(30),
-    Taille_categorie INTEGER CHECK (Taille_categorie BETWEEN 1 AND 5) NOT NULL, 
-    Volume INT NOT NULL CHECK (Volume >= 0),
-    Nombre_passagers INT NOT NULL CHECK (Nombre_passagers >= 0),
-    Initial_propietaire VARCHAR(30),
-    FOREIGN KEY (Initial_propietaire) REFERENCES NATIONS(Nationalite)
+CREATE TABLE navires(
+    navireID SERIAL PRIMARY KEY,
+    navire_type VARCHAR(30),
+    taille_categorie INTEGER CHECK (taille_categorie BETWEEN 1 AND 5) NOT NULL, 
+    volume INT NOT NULL CHECK (volume >= 0),
+    nombre_passagers INT NOT NULL CHECK (nombre_passagers >= 0),
+    initial_propietaire VARCHAR(30),
+    FOREIGN KEY (initial_propietaire) REFERENCES nations(nationalite)
 );
 
-CREATE TABLE Voyages(
-    NavireID INT,
-    Date_debut DATE,
-    Date_fin DATE,
-    Destination VARCHAR(30),
-    Type_voyage VARCHAR(30) CHECK (Type_voyage IN ('Court', 'Moyen', 'Long')),
-    Classe_voyage VARCHAR(30) CHECK (Classe_voyage IN ('Europe', 'Amérique', 'Asie', 'Intercontinental')),
-    FOREIGN KEY (NavireID) REFERENCES Navires(NavireID),
-    FOREIGN KEY (Destination) REFERENCES Ports_(Nom),
-    PRIMARY KEY (Date_debut, NavireID),
-    CHECK (Date_debut < Date_fin)
+CREATE TABLE voyages(
+    navireID INT,
+    date_debut DATE,
+    date_fin DATE,
+    destination VARCHAR(30),
+    type_voyage VARCHAR(30) CHECK (type_voyage IN ('Court', 'Moyen', 'Long')),
+    classe_voyage VARCHAR(30) CHECK (classe_voyage IN ('Europe', 'Amérique', 'Asie', 'Intercontinental')),
+    FOREIGN KEY (navireID) REFERENCES navires(navireID),
+    FOREIGN KEY (destination) REFERENCES ports_(nom),
+    PRIMARY KEY (date_debut, navireID),
+    CHECK (date_debut < date_fin)
 );
 
 /*CREATE TABLE Etapes_Transitoires(
@@ -60,45 +60,45 @@ CREATE TABLE Voyages(
     --TODO: Weak connection
 );*/
 
-CREATE TABLE Produits(
-    ProduitsID SERIAL PRIMARY KEY,
-    Nom VARCHAR(30),
-    TypeProduit VARCHAR (30) CHECK (TypeProduit IN ('Perissable', 'Sec', 'Personnes'))
+CREATE TABLE produits(
+    produitsID SERIAL PRIMARY KEY,
+    nom VARCHAR(30),
+    type_produit VARCHAR (30) CHECK (type_produit IN ('Perissable', 'Sec', 'Personnes'))
 );
 
-CREATE TABLE Quantite(
-    Etape_numero INT,
-    Date_debut DATE,
-    NavireID INT,
-    ProduitsID INT,
-    Quantite INT,
-    PRIMARY KEY (Etape_numero, Date_debut, NavireID, ProduitsID),
-    FOREIGN KEY(ProduitsID) REFERENCES Produits(ProduitsID),
-    FOREIGN KEY(NavireID) REFERENCES Navires(NavireID)
+CREATE TABLE quantite(
+    etape_numero INT,
+    date_debut DATE,
+    navireID INT,
+    produitsID INT,
+    quantite INT,
+    PRIMARY KEY (etape_numero, date_debut, navireID, produitsID),
+    FOREIGN KEY(produitsID) REFERENCES produits(produitsID),
+    FOREIGN KEY(navireID) REFERENCES navires(navireID)
 );
 
-CREATE TABLE Perissable(
-    ProduitsID INT PRIMARY KEY,
-    Date_conservation INTERVAL, --ex: SELECT date_conservation + DATE('2020-02-02') FROM Perissable;
-    Volume INT,
-    FOREIGN KEY(ProduitsID) REFERENCES Produits(ProduitsID)
+CREATE TABLE perissable(
+    produitsID INT PRIMARY KEY,
+    date_conservation INTERVAL, --ex: SELECT date_conservation + DATE('2020-02-02') FROM Perissable;
+    volume INT,
+    FOREIGN KEY(produitsID) REFERENCES produits(produitsID)
 );
 
-CREATE TABLE Sec(
-    ProduitsID INT PRIMARY KEY,
-    Volume INT,
-    FOREIGN KEY(ProduitsID) REFERENCES Produits(ProduitsID)
+CREATE TABLE sec(
+    produitsID INT PRIMARY KEY,
+    volume INT,
+    FOREIGN KEY(produitsID) REFERENCES produits(produitsID)
 );
 
-CREATE TABLE Personnes(
-    ProduitsID INT PRIMARY KEY,
-    FOREIGN KEY(ProduitsID) REFERENCES Produits(ProduitsID)
+CREATE TABLE personnes(
+    produitsID INT PRIMARY KEY,
+    FOREIGN KEY(produitsID) REFERENCES produits(produitsID)
 );
 
-CREATE TABLE Capturer (
-    Date_of_capture DATE,
-    NavireID INT,
-    Nationalite VARCHAR(30),
-    FOREIGN KEY(NavireID) REFERENCES Navires(NavireID),
-    FOREIGN KEY(Nationalite) REFERENCES Nations(Nationalite)
+CREATE TABLE capturer (
+    date_of_capture DATE,
+    navireID INT,
+    nationalite VARCHAR(30),
+    FOREIGN KEY(navireID) REFERENCES navires(navireID),
+    FOREIGN KEY(nationalite) REFERENCES nations(nationalite)
 );
