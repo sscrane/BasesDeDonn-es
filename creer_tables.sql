0948f6bcb5a98a96666e1acc1992624e0b0a4110
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-23 19:42:46 -0400

    added function constraint but commented out

--- creer_tables.sql
+++ creer_tables.sql
@@ -11,6 +11,7 @@ DROP TABLE IF EXISTS navires;
 DROP TABLE IF EXISTS ports_;
 DROP TABLE IF EXISTS nations;
 DROP TABLE IF EXISTS relations_diplomatiques;
+DROP FUNCTION IF EXISTS pleine_cale_ou_max_passagers;
 
 CREATE TABLE nations(
     nationalite VARCHAR(30) PRIMARY KEY,
@@ -59,6 +60,52 @@ CREATE TABLE etapes_transitoires(
         REFERENCES voyages (navireID, date_debut),
     FOREIGN KEY (port_nom) REFERENCES ports_(nom)
 );
+
+/*
+
+create function pleine_cale_ou_max_passagers()
+   returns INT 
+   language plpgsql
+  as
+$$
+begin
+
+    SELECT COUNT(*)
+    FROM etapes_transitoires AS e
+    NATURAL JOIN navires AS n
+    JOIN quantite AS q ON e.etape_numero = q.etape_numero AND e.navireID = q.navireID AND e.date_debut = q.date_debut
+    WHERE e.etape_numero = 0
+    AND ( 
+        -- calculate total volume of cargo
+        SELECT SUM(*) FROM (
+            -- list of quantite * volume of product
+            SELECT q3.quantite * p2.volume
+            FROM quantite AS q3 
+            JOIN produits AS p2 ON q3.produitsID = p2.produitsID
+            WHERE q3.navireID = e.navireID
+            AND q3.date_debut = e.date_debut
+            AND q3.etape_numero = e.etape_numero
+            AND p2.type_produit <> 'Personnes'
+        ) AS x) = n.volume
+    OR ( -- count de passagers 
+        SELECT COUNT(*) 
+        FROM etapes_transitoires AS e2
+        JOIN quantite AS q2 ON e2.etape_numero = q2.etape_numero AND e2.navireID = q2.navireID AND e2.date_debut = q2.date_debut
+        JOIN produits AS p ON q2.produitsID = p.produitsID
+        WHERE e2.etape_numero = e.etape_numero
+        AND e2.date_debut = e.date_debut
+        AND e2.navireID = e.navireID
+        AND p.type_produit = 'Personnes' ) = n.nombre_passagers
+    ;
+-- check for each etape 0, that either the cargo is full or there is a max num of passengers 
+end;
+$$;
+
+ALTER TABLE etapes_transitoires
+ADD CONSTRAINT pleine_cale_ou_max_passagers
+check (pleine_cale_ou_max_passagers() = 1);
+
+*/
 
 CREATE TABLE produits(
     produitsID SERIAL PRIMARY KEY,
