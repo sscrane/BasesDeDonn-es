95d945645986f4223beedf5586298eaa46fe60c4
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-26 17:13:40 -0400

    sasha's queries ideas

---
+++ REQ/req20.sql
@@ -0,0 +1,3 @@
+-- sasha
+
+-- what is the country that has owned the highest number of ships at one time? 
--- REQ/req8.sql
+++ REQ/req8.sql
@@ -1,1 +1,3 @@
 -- une requˆete impliquant le calcul de deux agr´egats (par exemple, les moyennes d’un ensemble de maximums)
+
+-- the average of (max volumes held on each ship at etape 0 per country)
