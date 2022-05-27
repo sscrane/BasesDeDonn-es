badd361d615e8660f2b8c1c655b5ce6bc6dd9eea
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-27 15:50:18 -0400

    req 7

--- REQ/req7.sql
+++ REQ/req7.sql
@@ -1,1 +1,8 @@
 -- something random 
+-- Get average volume per ship type per country 
+-- Le volume moyen transporté par type de navire par pays
+
+SELECT initial_propietaire, navire_type, ROUND(AVG(volume),2) AS average_volume FROM navires
+GROUP BY initial_propietaire, navire_type
+ORDER BY initial_propietaire, navire_type
+;
