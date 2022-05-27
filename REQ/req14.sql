c5544e7c2cf6ab5d7a2f5437e6ebce11c40ae9ae
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-27 15:26:55 -0400

    some work

--- REQ/req14.sql
+++ REQ/req14.sql
@@ -2,3 +2,21 @@
 -- bateau sur un laps de temps recouvrant des voyages diff´erents).
 
 -- route taken by ship 1 from 2020/01/01 to 2022/01/01
+
+WITH RECURSIVE nav_route(ville_départ, ville_arrivée) AS
+(
+SELECT * FROM voyages AS v
+NATURAL JOIN etapes_transitoires AS e
+WHERE v.navireID = 1
+AND v.date_debut > '2020-01-01'
+AND v.date_fin < '2022-01-01'
+
+UNION ALL
+
+SELECT V.ville_départ, A.ville_arrivée
+FROM Vol V, nav_route A
+WHERE V.ville_arrivée = A.ville_départ
+
+)
+SELECT * FROM nav_route
+LIMIT 1000 ;
--- REQ/req18.sql
+++ REQ/req18.sql
@@ -1,3 +1,7 @@
 -- sasha
 
 -- which nation has been at war with the most nations? 
+
+SELECT nation
+FROM relations_diplomatiques AS r
+JOIN relations_diplomatiques 
