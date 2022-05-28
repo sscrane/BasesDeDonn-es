0f3bc733da200c46d248d17929a0ec519d59d8ed
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-28 00:11:25 -0400

    recursive works hooray

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # BasesDeDonnes
 
-this is our project!
+Nom: Yeutseyeva
+Prénom: Sasha
+Numéro d'etudiant: 22121616
 
-For the readme I think it's worthwhile to read through the entire sujet and write down everything we did for each thing, noting things that didn't go as planned as well as our assumptions
+Nom: Crane
+Prénom: Sophie
+Numéro d'etudiant: ??
+
+creer_tables.sql:
+
+explique bri`evement et clairement ce que contient
+chaque fichier et dans quel ordre il faut ex´ecuter les diff´erents scripts pour cr´eer et
+alimenter la bases de donn´ees.
+
+copy_scripts.sql:
+
+REQ: 
+
+Autres points: 
--- REQ/req14.sql
+++ REQ/req14.sql
@@ -2,23 +2,34 @@
 -- bateau sur un laps de temps recouvrant des voyages diff´erents).
 
 -- route taken by ship 1 from 2020/01/01 to 2022/01/01
+
 \echo 
-\echo le trajet emprunté par le navire 1 du 2020/01/01 au 2022/01/01
+\echo Le trajet emprunté par le navire 1 du 2020/01/01 au 2022/01/01
 \echo 
-WITH RECURSIVE nav_route(ville_départ, ville_arrivée) AS
+
+
+-- voyage 1 destination
+
+-- etape 1 voyage 2 starting port 
+
+WITH RECURSIVE nav_route(port_depart, port_arrive,date_departure) AS
 (
-SELECT * FROM voyages AS v
+SELECT e.port_nom AS port_depart, v.destination AS port_arrive, v.date_debut AS date_departure FROM voyages AS v
 NATURAL JOIN etapes_transitoires AS e
 WHERE v.navireID = 1
-AND v.date_debut > '2020-01-01'
-AND v.date_fin < '2022-01-01'
+AND e.etape_numero = 1
+AND v.date_debut >= (SELECT MIN(v2.date_debut) FROM voyages AS v2 WHERE navireID = 1 AND v2.date_debut > '2019-12-31')
 
 UNION ALL
 
-SELECT V.ville_départ, A.ville_arrivée
-FROM Vol V, nav_route A
-WHERE V.ville_arrivée = A.ville_départ
+SELECT v.destination AS port_depart, A.port_arrive AS port_arrive, v.date_debut AS date_departure
+FROM voyages AS v NATURAL JOIN etapes_transitoires AS e, nav_route A
+WHERE e.port_nom = A.port_depart
+AND e.etape_numero = 1
+AND v.navireID = 1
+AND v.date_fin < '2022-01-01'
+AND v.date_debut NOT IN (date_departure)
 
 )
 SELECT * FROM nav_route
-LIMIT 1000 ;
+LIMIT 10 ;
