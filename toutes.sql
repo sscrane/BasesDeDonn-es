6b19d631aa99aae48b85c87034f3b19fe9756adb
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-29 14:48:49 -0400

    capturer fixed

--- toutes.sql
+++ toutes.sql
@@ -131,8 +131,17 @@ ORDER BY v.date_debut ASC ;
 \echo
 \echo REQUETE 10: Ports qui ont été visités par un navire de chaque continent 
 \echo
-SELECT DISTINCT e.port_nom
-FROM etapes_transitoires AS e
+SELECT DISTINCT x.port_noms
+FROM (
+    SELECT e.port_nom AS port_noms
+    FROM etapes_transitoires AS e
+
+    UNION 
+
+    SELECT v.destination AS port_noms
+    FROM voyages AS v
+        
+) AS x 
 WHERE NOT EXISTS (
     SELECT * FROM nations AS n
     WHERE n.continent NOT IN 
@@ -143,8 +152,19 @@ WHERE NOT EXISTS (
         JOIN navires AS n2 ON e2.navireID = n2.navireID
         JOIN capturer AS c ON n2.navireID = c.navireID
         JOIN nations AS nat ON c.nationalite = nat.nationalite
-        WHERE e2.port_nom = e.port_nom
-        AND c.date_of_capture = ( SELECT MAX(c5.date_of_capture) FROM capturer AS c5 WHERE c5.navireID = c.navireID AND c5.date_of_capture < e2.date_debut)   
+        WHERE e2.port_nom = x.port_noms
+        AND c.date_of_capture = ( SELECT MAX(c5.date_of_capture) FROM capturer AS c5 WHERE c5.navireID = c.navireID AND c5.date_of_capture < e2.date_debut)  
+
+        UNION
+
+        SELECT nat.continent 
+        FROM voyages AS v2
+        JOIN navires AS n2 ON v2.navireID = n2.navireID
+        JOIN capturer AS c ON n2.navireID = c.navireID
+        JOIN nations AS nat ON c.nationalite = nat.nationalite
+        WHERE v2.destination  = x.port_noms
+        AND c.date_of_capture = ( SELECT MAX(c5.date_of_capture) FROM capturer AS c5 WHERE c5.navireID = c.navireID AND c5.date_of_capture < v2.date_debut)
+
     )
 )
 ;
