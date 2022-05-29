5ad7f9e73a508ea883db4f25993e82809507f654
Sasha Yeutseyeva <sashayeutseyeva@gmail.com>
2022-05-28 23:25:43 -0400

    request 3 and 17 satisfied

--- REQ/req17.sql
+++ REQ/req17.sql
@@ -2,7 +2,8 @@
 \echo 
 \echo Différents navires partant et arrivant les mêmes jours
 \echo 
-SELECT * FROM voyages v1, voyages v2 
+SELECT v1.navireID,v2.navireID,v1.date_debut,v1.date_fin 
+FROM voyages v1, voyages v2 
 WHERE v1.navireid <> v2.navireid 
 AND v1.date_debut = v2.date_debut 
 AND v1.date_fin = v2.date_fin;
